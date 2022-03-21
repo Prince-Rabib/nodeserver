6a03df7e05f1a7eb6c5a7c642a4a65b8c47760a1
Prince-Rabib <rabibhossain123@gmail.com>
2022-03-22 00:09:34 +0600

    u

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log('Server started on port ' + PORT);
 });
-
-
 app.use(cors());
 app.use(express.json({ extended: false }));
 
