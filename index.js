9a20b73a54a986792d615caed9541d4c938cf214
rabib <rabibhossain123@gmail.com>
2023-06-23 01:53:12 +0600

    little changes

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const appointment = require('./api/appointment');
+const mechanic = require('./api/mechanics');
+const cors = require('cors');
+
+const connectdb = require('./config/db');
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json({ extended: false }));
+
+connectdb();
+
+app.get('/', (req, res) => {
+  return res.send('app is running');
+});
+
+
+app.use('/api/mechanics', mechanic);
+app.use('/api/appointment', appointment);
+
+const PORT = process.env.PORT || 8000;
+
+const server = app.listen(process.env.PORT || 8000, () => {
+  const port = server.address().port;
+  console.log(`Express is working on port ${port}`);
+});
